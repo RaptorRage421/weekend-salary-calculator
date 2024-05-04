fd6f52c9d8f0558c4387d8231aa0277645db8432
Sam Shacter <sshacter@gmail.com>
2024-05-04 10:03:31 -0500

    trying to locate an identifier for tracking which person is deleted from the table.

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ function handleSubmit(event) {
     let titleInput = document.getElementById('titleForm').value;
     let annualSalaryInput = document.getElementById('annualSalaryForm').value;
 
-console.log('name is: ', firstNameInput + ' ' + lastNameInput)
+    console.log('name is: ', firstNameInput + ' ' + lastNameInput)
+    console.log('event: ', event, EventSource, EventTarget)
 
-let tableContents = document.getElementById('table_content');
-tableContents.innerHTML += `
+    let tableContents = document.getElementById('table_content');
+    tableContents.innerHTML += `
 <tr>
 <td>${firstNameInput}</td>
 <td>${lastNameInput}</td>
@@ -22,32 +23,35 @@ tableContents.innerHTML += `
 <td><button onClick="deleteRow(event)"> Delete </button></td>
 </tr>
 `
-document.getElementById('firstNameForm').value = ""
-document.getElementById('lastNameForm').value = ""
-document.getElementById('idForm').value = ""
-document.getElementById('titleForm').value = ""
-document.getElementById('annualSalaryForm').value = ""
-let footerAlert = document.getElementById("footer_alert")
-console.log('class list: ', footerAlert.classList)
-console.log('footer alert has a value: ', footerAlert)
-let monthlySalary = annualSalaryInput/12
-let totalMonthly = document.getElementById('total_monthly')
-monthlyCost += monthlySalary
-totalMonthly.innerHTML = monthlyCost
-if (monthlyCost > 20000){
-    
-    footerAlert.classList.add("over-budget")
-    
-}
-console.log('footerAlert classlist: ', footerAlert.classList)
+    document.getElementById('firstNameForm').value = ""
+    document.getElementById('lastNameForm').value = ""
+    document.getElementById('idForm').value = ""
+    document.getElementById('titleForm').value = ""
+    document.getElementById('annualSalaryForm').value = ""
+    let footerAlert = document.getElementById("footer_alert")
+    let footerExtra = document.getElementById("special-add")
+    console.log('class list: ', footerAlert.classList)
+    console.log('footer alert has a value: ', footerAlert)
+    let monthlySalary = annualSalaryInput / 12
+    let totalMonthly = document.getElementById('total_monthly')
+    monthlyCost += monthlySalary
+    totalMonthly.innerHTML = monthlyCost
+    if (monthlyCost > 20000) {
+
+        footerAlert.classList.add("over-budget","overbudget")
+        // footerExtra.classList.add("overbudget")
+      
+    }
+    console.log('footerAlert classlist: ', footerAlert.classList)
 }
 
 
-function deleteRow(event){
-console.log('delete function is working')
-console.log(event)
-let deleteTableRow = event.target.parentElement.parentElement
-deleteTableRow.remove()
+function deleteRow(event) {
+    console.log('delete function is working')
+    console.log(event)
+    let deleteTableRow = event.target.parentElement.parentElement
+    console.log('delete event info, tracking data: ', event.target.parentElement.parentElement.t)
+    deleteTableRow.remove()
 
 }
 
